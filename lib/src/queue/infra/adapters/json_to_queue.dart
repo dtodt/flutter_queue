85913e59bd8df761f78a757a2d3adac1a8ff5ab1
dtodt <danieltodt@gmail.com>
2022-04-01 17:33:03 -0300

    chore queue mapper

---
+++ lib/src/queue/infra/adapters/json_to_queue.dart
@@ -0,0 +1,21 @@
+import 'package:queue/src/queue/domain/entities/queue_entity.dart';
+import 'package:queue/src/queue/infra/adapters/json_to_order.dart';
+
+/// Queue json mapper
+class JsonToQueue {
+  static QueueEntity fromMap(dynamic json) {
+    return QueueEntity(
+      uid: json['uid'],
+      title: json['title'],
+      acronym: json['acronym'],
+      priority: json['priority'],
+      orders: json.containsKey('orders')
+          ? JsonToOrder.fromList(json['orders'] as List)
+          : [],
+    );
+  }
+
+  static List<QueueEntity> fromList(List<dynamic> list) {
+    return list.map((json) => fromMap(json)).toList();
+  }
+}
---
+++ test/src/queue/infra/adapters/json_to_queue_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:queue/src/queue/domain/entities/queue_entity.dart';
+import 'package:queue/src/queue/infra/adapters/json_to_queue.dart';
+
+import 'json_to_order_test.dart';
+
+const kEmptyQueueMap = {
+  'uid': 'uid',
+  'title': 'title',
+  'acronym': 'acronym',
+  'priority': 1,
+};
+
+const kQueueMap = {
+  ...kEmptyQueueMap,
+  'orders': [kOrderMap],
+};
+
+void main() {
+  test('should convert a map into queue', () async {
+    final result = JsonToQueue.fromMap(kQueueMap);
+    expect(result, isA<QueueEntity>());
+    expect(result.orders, isNotEmpty);
+    expect(result.priority, equals(1));
+  });
+
+  test('should convert a map into queue when no orders is present', () async {
+    final result = JsonToQueue.fromMap(kEmptyQueueMap);
+    expect(result, isA<QueueEntity>());
+    expect(result.orders, isEmpty);
+  });
+
+  test('should convert a list of map into a list of queue', () async {
+    final result = JsonToQueue.fromList([kQueueMap]);
+    expect(result, isA<List<QueueEntity>>());
+  });
+}
