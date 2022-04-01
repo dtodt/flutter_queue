20ac289163d76afa2156e9393cf842ce1376fd14
dtodt <danieltodt@gmail.com>
2022-04-01 18:14:35 -0300

    chore firebase added

---
+++ lib/src/queue/external/queue_firestore_datasource.dart
@@ -0,0 +1,9 @@
+import 'package:queue/src/queue/infra/datasources/queue_datasource.dart';
+
+class QueueFirestoreDatasource implements IQueueDatasource {
+  @override
+  Stream<List<Map>> getAllQueues() {
+    // TODO: implement getAllQueues
+    throw UnimplementedError();
+  }
+}
